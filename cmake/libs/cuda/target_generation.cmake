438f9ba678f56253a7bc0cb533c967ac936b3dfe
Albert Andaluz <albertandaluz@gmail.com>
2026-06-16 20:28:12 +0200

    pass cpp mode

--- cmake/libs/cuda/target_generation.cmake
+++ cmake/libs/cuda/target_generation.cmake
@@ -1,15 +1,16 @@
 function(set_default_cuda_target_properties TARGET_NAME)
     if (WIN32)
-        list(APPEND COMPILER_CUDA_FLAGS "-Xcompiler=/bigobj" "-Xcompiler=/Zc:preprocessor")
+        list(APPEND COMPILER_CUDA_FLAGS "-Xcompiler=/bigobj" "-Xcompiler=/Zc:preprocessor /std:c++23preview") #for utf8 codepage
     endif()
     target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:${COMPILER_CUDA_FLAGS}>)
-    if(CUDA_VERSION_MAJOR GREATER_EQUAL 13)	
-        set_target_properties(${TARGET_NAME} PROPERTIES CUDA_STANDARD_REQUIRED ON CUDA_STANDARD 20 CUDA_RUNTIME_LIBRARY
-                                                                                               Hybrid)
-    else()
-    set_target_properties(${TARGET_NAME} PROPERTIES CUDA_STANDARD_REQUIRED ON CUDA_STANDARD 20 CUDA_RUNTIME_LIBRARY
-                                                                                               Shared)
+    #c++23 only supported for cuda 13.3 and higher, so we need to check the version
+    if (CUDA_VERSION_MAJOR GREATER_EQUAL 13 AND CUDA_VERSION_MINOR LESS 3)
+        message(ERROR "CUDA version ${CUDA_VERSION} is not supported. Please use CUDA 13.3 or higher.")
     endif()
+    
+    set_target_properties(${TARGET_NAME} PROPERTIES CUDA_STANDARD_REQUIRED ON CUDA_STANDARD 23 CUDA_RUNTIME_LIBRARY
+                                                                                               Hybrid)
+    
     
     set_target_cuda_arch_flags(${TARGET_NAME})
     
