8fbc6e017400536248032bf367af159d8fb401af
Oscar Amoros Huguet <morousg@gmail.com>
2026-06-27 12:28:42 +0200

    Recovering the bug tests and softening cmake requirements. We do not require CUDA 13, we WARN that we only test with CUDA 13. The moment we migrate to C++23, then we will require the minimum CUDA version that supports C++23.

--- cmake/libs/cuda/target_generation.cmake
+++ cmake/libs/cuda/target_generation.cmake
@@ -2,16 +2,13 @@ function(set_default_cuda_target_properties TARGET_NAME)
     if (WIN32)
         list(APPEND COMPILER_CUDA_FLAGS "-Xcompiler=/bigobj" "-Xcompiler=/Zc:preprocessor")
     endif()
-    target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:${COMPILER_CUDA_FLAGS}>)
     
-    if (NOT CMAKE_CUDA_COMPILER_ID STREQUAL "NVIDIA")
-        message(FATAL_ERROR "Only NVCC is supported as the CUDA compiler (CMAKE_CUDA_COMPILER_ID=${CMAKE_CUDA_COMPILER_ID}).")
-    endif()
+	target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:${COMPILER_CUDA_FLAGS}>)
 
     if (CUDA_VERSION_MAJOR LESS 13)
-        message(FATAL_ERROR "CUDA 13+ is required (CUDA_VERSION_MAJOR=${CUDA_VERSION_MAJOR}).")
+        message(WARNING "We only test with CUDA 13+ (CUDA_VERSION_MAJOR=${CUDA_VERSION_MAJOR}).")
     endif()
-
+	
     set_target_properties(${TARGET_NAME} PROPERTIES CUDA_STANDARD_REQUIRED ON CUDA_STANDARD 20 CUDA_RUNTIME_LIBRARY Hybrid)
     
     set_target_cuda_arch_flags(${TARGET_NAME})
@@ -19,27 +16,27 @@ function(set_default_cuda_target_properties TARGET_NAME)
     # use less precise but faster cuda math methods
     #target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:--use_fast_math>)
     # parallel compilation of cuda kernels
-    target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:--threads 0>)
-    #disable relocatable device code   
-    #see https://forums.developer.nvidia.com/t/the-cost-of-relocatable-device-code-rdc-true/47665
-    target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-rdc=false>)
-       
-    #split compile does not work with gpu debug code
-    if(NOT ${ENABLE_DEBUG})
-        target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-split-compile=0>)
-    endif()
-        
      
+	target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:--threads 0>)
+	#disable relocatable device code   
+    #see https://forums.developer.nvidia.com/t/the-cost-of-relocatable-device-code-rdc-true/47665
+	target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-rdc=false>)
+	#split compile does not work with gpu debug code
+	if(NOT ${ENABLE_DEBUG})
+		target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-split-compile=0>)
+	endif()
+
     if (NOT(${TEMPLATE_DEPTH} STREQUAL  "default"))
     #bugfix for windows compilation of tests with more than 200 recursions
     #set it in device code and host
         target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-ftemplate-depth=${TEMPLATE_DEPTH}>)
 
         if (WIN32)
-            #cannot be set, no compiler flag available
+			#cannot be set, no compiler flag available
         else() 
-            #asume clang or gcc on linux
-            target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-ftemplate-depth=${TEMPLATE_DEPTH}>)           
+			#asume clang or gcc on linux
+			target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CXX>:-ftemplate-depth=${TEMPLATE_DEPTH}>)           
         endif()
     endif()
+	
 endfunction()
