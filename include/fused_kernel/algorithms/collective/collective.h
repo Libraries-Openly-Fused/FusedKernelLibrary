aefc0b8635d413550cec6232f867aa39c870489c
johnnynunez <johnnynuca14@gmail.com>
2026-06-28 12:50:10 +0200

    feat(collective): add warp tile scheduler op

--- include/fused_kernel/algorithms/collective/collective.h
+++ include/fused_kernel/algorithms/collective/collective.h
@@ -20,5 +20,6 @@
 #include <fused_kernel/algorithms/collective/async_copy.h>
 #include <fused_kernel/algorithms/collective/mma.h>
 #include <fused_kernel/algorithms/collective/mainloop.h>
+#include <fused_kernel/algorithms/collective/tile_scheduler.h>
 
 #endif // FK_COLLECTIVE
