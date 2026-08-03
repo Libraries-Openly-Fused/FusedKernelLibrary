1838493233178900229304c657fb7fb0ff79cbf6
Oscar Amoros Huguet <morousg@gmail.com>
2026-08-04 00:41:33 +0200

    Fixed issue with CircularTensor. Not a definitive solution, but works

--- tests/data/test_circular_tensor.h
+++ tests/data/test_circular_tensor.h
@@ -14,6 +14,76 @@
 
 #include "tests/main.h"
 
+#include <fused_kernel/algorithms/basic_ops/memory_operations.h>
+#include <fused_kernel/algorithms/image_processing/saturate.h>
+#include <fused_kernel/core/data/circular_tensor.h>
+#include <fused_kernel/core/data/ptr_utils.h>
+#include <fused_kernel/fused_kernel.h>
+
+#include <iostream>
+
+template <uint BATCH, uint WIDTH, uint HEIGHT, uint ITERS, typename IT, typename OT>
+bool testCircularTensor() {
+    using TensorOT = typename fk::VectorTraits<OT>::base;
+    constexpr uint COLOR_PLANES = fk::cn<IT>;
+
+    fk::CircularTensor<TensorOT, COLOR_PLANES, BATCH, fk::CircularTensorOrder::NewestFirst, fk::ColorPlanes::Standard>
+        myTensor(WIDTH, HEIGHT);
+    fk::Ptr2D<IT> input(WIDTH, HEIGHT);
+
+    fk::Stream fk_stream;
+    fk::setTo(10.0f, myTensor, fk_stream);
+
+    for (int i = 0; i < ITERS; i++) {
+        fk::setTo(fk::make_<IT>(i + 1, i + 1, i + 1), input, fk_stream);
+        myTensor.update(fk_stream, fk::Read<fk::PerThreadRead<fk::ND::_2D, IT>>{input.ptr()},
+                        fk::Unary<fk::SaturateCast<IT, OT>>{}, fk::Write<fk::TensorSplit<OT>>{myTensor.ptr()});
+        fk_stream.sync();
+    }
+
+    myTensor.download(fk_stream);
+    fk_stream.sync();
+
+    bool correct = true;
+    for (int z = 0; z < BATCH; z++) {
+        const TensorOT value = (TensorOT)(ITERS - z);
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                const fk::Point p{x, y, z};
+                const TensorOT res = *fk::PtrAccessor<fk::ND::_3D>::point(p, myTensor.ptrPinned());
+                correct &= value == res;
+            }
+        }
+    }
+
+    return correct;
+}
+
+template <uint BATCH, uint WIDTH, uint HEIGHT, uint ITERS, typename IT, typename OT>
+void launchTest() {
+    if (testCircularTensor<BATCH, WIDTH, HEIGHT, ITERS, IT, OT>()) {
+        std::cout << "testCircularTensor<" << BATCH << ", " << WIDTH << ", " << HEIGHT << ", " << ITERS << ", " << typeid(IT).name() << ", " << typeid(OT).name() << "> OK" << std::endl;
+    } else {
+        std::cout << "testCircularTensor<" << BATCH << ", " << WIDTH << ", " << HEIGHT << ", " << ITERS << ", " << typeid(IT).name() << ", " << typeid(OT).name() << "> Failed!"
+                  << std::endl;
+    }
+}
+
 int launch() {
-    return 0;
+    int returnValue = 0;
+    launchTest<2, 128, 128, 100, uchar3, float3>();
+    launchTest<3, 128, 128, 100, uchar3, float3>();
+    launchTest<4, 128, 128, 100, uchar3, float3>();
+    launchTest<5, 128, 128, 100, uchar3, float3>();
+    launchTest<6, 128, 128, 100, uchar3, float3>();
+    launchTest<7, 128, 128, 100, uchar3, float3>();
+    launchTest<8, 128, 128, 100, uchar3, float3>();
+    launchTest<9, 128, 128, 100, uchar3, float3>();
+    launchTest<10, 128, 128, 100, uchar3, float3>();
+    launchTest<11, 128, 128, 100, uchar3, float3>();
+    launchTest<12, 128, 128, 100, uchar3, float3>();
+    launchTest<13, 128, 128, 100, uchar3, float3>();
+    launchTest<14, 128, 128, 100, uchar3, float3>();
+    launchTest<15, 128, 128, 100, uchar3, float3>();
+    return returnValue;
 }
