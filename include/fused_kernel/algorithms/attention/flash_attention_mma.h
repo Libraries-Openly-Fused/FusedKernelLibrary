be4cef38fa452aec96035922ee2457d8f927e2da
Oscar Amoros Huguet <morousg@gmail.com>
2026-09-13 18:48:01 +0200

    Update copyright information in flash_attention_mma.h

--- include/fused_kernel/algorithms/attention/flash_attention_mma.h
+++ include/fused_kernel/algorithms/attention/flash_attention_mma.h
@@ -1,4 +1,5 @@
-/* Copyright 2026 the Fused Kernel Library authors
+/* Copyright 2026 Oscar Amoros Huguet
+   Copyright 2026 Johnny Nunez
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
